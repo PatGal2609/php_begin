75b9f5ca55095c8c3fb9bfc4902d4aaa26fa5ff9
Freddy <paterson.nya@gmail.com>
2023-12-13 01:12:57 +0300

    Начала

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP задания 1</h1>
+    <p>
+    Создайте файл с формой, содержащей следующие поля: фамилия, имя, отчество, отдел, должность. Отдел предлагается выбрать из выпадающего списка. Варианты: ИТ, Бухгалтерия, Продаж, Логистика. 
+    </p>
+    
+</body>
+</html>
